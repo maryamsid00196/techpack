ca20148571f78447f04d0b4e8641c3c34792bdf6
Maryam Siddiqui <90316975+maryamsid00196@users.noreply.github.com>
2025-09-03 14:27:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -139,13 +139,18 @@ if cap_file:
             cap.save(cap_path)
     else:
         cap = load_image(cap_path)
-
+    
+    max_w, max_h = 800, 600
+    w = min(cap.width, max_w)
+    h = min(cap.height, max_h)
 
     canvas_result = st_canvas(
            fill_color="rgba(255, 165, 0, 0.3)",
            stroke_width=2,
            stroke_color="red",
-           background_image=cap, 
+           background_image=cap,
+           width=w,
+           height=h, 
            update_streamlit=True,
            #height=display_size[1],
            #width=display_size[0],
@@ -207,6 +212,7 @@ if st.session_state.results:
         generate_pdf_report(st.session_state.results, "logo_techpack.pdf")
         with open("logo_techpack.pdf", "rb") as f:
             st.download_button("⬇️ Download Techpack PDF", f, file_name="logo_techpack.pdf")
+
 
 
 
