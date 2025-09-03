bc1226d2f61d69b54c29a7b079ddc9a6fdf930a5
Maryam Siddiqui <90316975+maryamsid00196@users.noreply.github.com>
2025-09-03 15:00:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ if cap_file:
         fill_color="rgba(255, 165, 0, 0.3)",
         stroke_width=2,
         stroke_color="red",
-        background_image=cap_resized1,
+        background_image=cap_resized,
         width=w,
         height=h,
         update_streamlit=True,
@@ -222,4 +222,5 @@ if st.session_state.results:
         generate_pdf_report(st.session_state.results, "logo_techpack.pdf")
         with open("logo_techpack.pdf", "rb") as f:
             st.download_button("⬇️ Download Techpack PDF", f, file_name="logo_techpack.pdf")
+
 
